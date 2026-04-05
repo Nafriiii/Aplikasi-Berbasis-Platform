c3c1b1005cf6be2f4bd2caf8b6c5617c635ea2a3
Nafriiii <email.irfanthoriq012@gmail.com>
2026-04-05 17:11:13 +0700

    cek

--- teori ABP/Tutorial_Laravel2_2311102131_Irfan Thoriq Habibi/laravel2/routes/web.php
+++ teori ABP/Tutorial_Laravel2_2311102131_Irfan Thoriq Habibi/laravel2/routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route; 
 
+Route::redirect('/', '/login');
+
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/auth', [AuthController::class, 'auth'])->name('auth');
 
